fb6e26871df6faa8dec6eddb3ccecbb882908489
Germain Boue <boue.germain@gmail.com>
2015-10-09 21:57:22 -0700

    Create the ScatterPlot Component

---
+++ app/components/ScatterPlot.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+var DataCircles = require('./DataCircles');
+
+var ScatterPlot = React.createClass({
+  render: function(){
+    var props = this.props;
+    var xScale = this.getXScale(props);
+    var yScale = this.getYScale(props);
+    return(
+      <svg width={props.width} height={props.height}>
+        <DataCircles
+          xScale={xScale}
+          yScale={yScale}
+          {...props} />
+      </svg>
+    );
+  },
+
+  getXScale: function(props){
+    //d3.max is a d3 method for determining the maximum value of a dataset.
+    var xMax = d3.max(props.data, function(d) { return d[0] });
+    return d3.scale.linear()
+      .domain([0, xMax])
+      .range([props.padding, props.width - props.padding * 2]);
+  },
+
+  getYScale: function(props){
+    var yMax = d3.max(props.data, function(d) { return d[1] });
+        return d3.scale.linear()
+          .domain([0, yMax])
+          .range([props.height - props.padding, props.padding]);
+  },
+});
+
+module.exports = ScatterPlot;
