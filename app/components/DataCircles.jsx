f4e7823d7db7b00aad985714177927f08d4ad6b7
Germain Boue <boue.germain@gmail.com>
2015-10-09 22:02:16 -0700

    Create DataCircle component

---
+++ app/components/DataCircles.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var uuid = require('node-uuid');
+
+var DataCircles = React.createClass({
+  render: function(){
+    //we're rendering a g element, which is like the SVG equivalent to a div
+    //Inside of g, we're mapping over the data and rendering a circle for 
+    //each one using renderCircles.
+    return <g> {this.props.data.map(this.renderCircles) } <g>
+  },
+
+  //renderCircles creates an SVG circle element which takes a number of properties.
+  renderCircles: function(coords){
+    var props = {
+      cx: this.props.xScale(coords[0]),
+      cy: this.props.yScale(coords[1]),
+      r: 2,
+      //React's diffing algorithm needs some kind of way to keep track of them as it updates the DOM over and over. 
+      //I'm using node-uuid because it's an easy way to create a unique key for each element
+      key: uuid.v4()
+    };
+    return (
+      <circle {...props}>
+      </circle>
+    );
+  }
+});
+
+module.exports = DataCircles;
